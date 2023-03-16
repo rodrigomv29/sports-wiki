c36bda788b4a458a5b02f3ba82acf9ff6658d76b
rodrigomv29 <43789876+rodrigomv29@users.noreply.github.com>
2023-03-15 20:04:34 -0400

    edit boxer.js

--- Boxer.js
+++ Boxer.js
@@ -1,9 +1,23 @@
 const combatathlete = require('./CombatAthlete')
 class Boxer extends combatathlete {
-    constructor(stance, style){
+    constructor(stance, style="technical"){
         super();
+        this.stance = stance;
+        this.style = style;
 
+    }
+    getStance(){
+        return this.stance;
+    }
+    setStance(stance){
+        this.stance = stance;
+    }
+    getStyle(){
+        return this.style;
+    }
+    setStyle(style){
+        this.style = style;
     }
 
 }
-module.exports = combatathlete;
+module.exports = Boxer;
