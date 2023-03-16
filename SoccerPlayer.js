fbdf23fa4dfa3eb2e56eb9d73499cd9b1b85d324
rodrigomv29 <43789876+rodrigomv29@users.noreply.github.com>
2023-03-15 21:34:36 -0400

    committed soccer constructor

---
+++ SoccerPlayer.js
@@ -0,0 +1,11 @@
+class SoccerPlayer extends Athlete{
+    constructor(name, number, position){
+        super();
+        this.name = name;
+        this.number = number;
+        this.position = position;
+
+    }
+
+}
+module.exports = SoccerPlayer;
