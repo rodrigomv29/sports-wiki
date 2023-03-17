42afa2880ddb6216545906b5b5228fa28f0367e9
rodrigomv29 <43789876+rodrigomv29@users.noreply.github.com>
2023-03-17 18:03:00 -0400

    added indexRouter and ejs template

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const indexRouter = require('./routes/indexRouter.js')
+
+const app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.use("/", indexRouter)
+
+
+
