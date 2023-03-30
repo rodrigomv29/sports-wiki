d693c16d78d246ce752808d7516c5cf6a569724c
rodrigomv29 <43789876+rodrigomv29@users.noreply.github.com>
2023-03-30 16:57:24 -0400

    undo

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 const express = require('express');
+const indexRouter = require('./routes/index.js')
+const { auth } = require('express-openid-connect');
+require("dotenv").config();
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
 
 const app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(auth(config));
+
+
+app.use("/", indexRouter)
+
+app.listen(3000, () => {
+    console.log("Express running on port 3000");
+    // express
+    // view wngine unknown
+
+    /**
+     * undocumented
+     */
+})
