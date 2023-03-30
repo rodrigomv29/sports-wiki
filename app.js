a0d0416a6fe16568ff340d6115cb325ee57a07e3
rodrigomv29 <43789876+rodrigomv29@users.noreply.github.com>
2023-03-30 16:44:40 -0400

    hello world (covey town)

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
 const express = require('express');
-const indexRouter = require('./routes/index.js')
 
 const app = express();
-
-app.set('views', 'views');
-app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
-
-app.use("/", indexRouter)
-
-app.listen(3000, () => {
-    console.log("Express running on port 3000");
-    // express
-    // view wngine unknown
-
-    /**
-     * undocumented
-     */
-})
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render("index", {title: "Express"});
+    //console.log(req.oidc.isAuthenticated());
+    console.log(req.oidc.user);
+    res.render("index", {
+        title: "Express Demo",
+        isAuthenticated: req.oidc.isAuthenticated(),
+        user: req.oidc.user
 
+    })
 })
 
 module.exports = router;
